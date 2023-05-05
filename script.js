5c1e95b64f5d2dc3573cb35e4f797e2e201fdcda
wongpakho814 <wongpakho814@gmail.com>
2023-05-05 17:36:48 +1000

    Implemented scroll button

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ document.querySelector(".hamburger-menu").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");  
 });
 // End of sidebar
+
+// Scroll button
+  document.querySelector(".scroll-btn").addEventListener("click", () => {
+    document.querySelector("html").style.scrollBehavior = "smooth";
+    setTimeout(() => {
+      document.querySelector("html").style.scrollBehavior = "unset";    
+    }, 1000);
+  });
+// End of scroll button
